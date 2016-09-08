18d9899072a08a453ec253afd68f644ab85c4ef3
Domsche <stecher.do@gmail.com>
2016-09-08 17:15:10 +0200

    Include eingefügt und doppelten Code gelöscht

--- Registration.php
+++ Registration.php
@@ -8,16 +8,7 @@
 	  <img src="Images/background.jpg"> <!-- Wird Targilogo-Hintergrund-->
 	  <main>
 	    <header>Registrierung</header>
-		  <form>
-		  <!-- Hier kann der User später seine E-mail, Username etc. eingeben -->
-		  </form>
-		  <article>
-		    <img src="Images/captcha.jpg"> <!-- Hier wird ein captcha-Bild später angezeigt -->
-		      <form>
-		        <!-- Hier wird die captcha eingegeben -->
-		      </form>
-		  </article>
-		  <!-- Hier wird die Schaltfläche für den Bestätigen Button sein -->
+		 <?php include("Gruppen_de/registration.php") ?>
 		</main>
 	</body>
 </html>
