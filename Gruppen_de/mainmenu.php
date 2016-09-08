443a303097e660e2a5f551a740edbd432ad1bf0e
Domsche <stecher.do@gmail.com>
2016-09-08 16:58:40 +0200

    Erster Entwurf

--- Gruppen_de/mainmenu.php
+++ Gruppen_de/mainmenu.php
@@ -1,1 +1,15 @@
+<?php
 
+<form>
+<input class="NewGame" type="button" value="Neues Spiel" onclick="StartinGgame.php" /> //Hier wusste ich nicht wie der Pfad exakt aussehen muss am Ende
+</form>
+
+
+<form>
+<input class="NewGame" type="button" value="Regeln" onclick="rules.php" /> 
+</form>
+
+
+<form>
+<input class="NewGame" type="button" value="Log Out" onclick="logout.php" /> //Diese Gruppe habe ich noch nicht erstellt, wird nachgeholt
+</form>
