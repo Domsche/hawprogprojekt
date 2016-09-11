c36f16d811e780a59c95dd5c2749ca2f08f6b5aa
Domsche <stecher.do@gmail.com>
2016-09-11 16:30:17 +0200

    Erster Entwurf

--- PlayingSurface.php
+++ PlayingSurface.php
@@ -1,1 +1,21 @@
+<! doctype html>
+<html lang=de>
+	<head>
+		<title>PlayingSurface</title>
+		<meta charste=utf-8>
+	</head>
+	<body>
+	  <main>
+	    <header>
+	      <h1>TARGI - THE GAME</h1>
+	    </header>
+	  <img src="Images/background.jpg"> <!-- Wird Targilogo-Hintergrund-->
+	  <?php include("Gruppen_de/navigation.php") ?>
+	  <?php include("Gruppen_de/gamestats.php") ?>
+	  <?php include("Gruppen_de/cards.php") ?>
+	  <?php include("Gruppen_de/field.php") ?>
+	
+		</main>
+	</body>
+</html>
 
