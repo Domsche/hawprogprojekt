2d31c8a10461d41def51a4a58e1d2cac866b41b0
Domsche <stecher.do@gmail.com>
2016-09-11 17:51:17 +0200

    CSS Anbindung

--- PlayingSurface.php
+++ PlayingSurface.php
@@ -3,19 +3,19 @@
 	<head>
 		<title>PlayingSurface</title>
 		<meta charste=utf-8>
+		<link rel=stylesheet href=styles.css>
 	</head>
 	<body>
 	  <main>
 	    <header>
 	      <h1>TARGI - THE GAME</h1>
 	    </header>
-	  <img src="Images/background.jpg"> <!-- Wird Targilogo-Hintergrund-->
 	  <?php include("Gruppen_de/navigation.php") ?>
 	  <?php include("Gruppen_de/gamestats.php") ?>
 	  <?php include("Gruppen_de/cards.php") ?>
 	  <?php include("Gruppen_de/field.php") ?>
 	
-		</main>
+         </main>
 	</body>
 </html>
 
