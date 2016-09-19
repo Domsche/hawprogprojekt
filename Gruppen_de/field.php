2c82d57701768f37af3aaacc915f1c480d8cf47b
Domsche <stecher.do@gmail.com>
2016-09-19 17:25:44 +0200

    Erster Entwurf

--- Gruppen_de/field.php
+++ Gruppen_de/field.php
@@ -1,1 +1,13 @@
+<table>
+<?php
+$playeramount = <!--Startwert ist 0, pro Spieler +1  -->
 
+for ($i = 0; $i < $playeramount + 5; $i++) {
+    echo '<tr>';
+    for ($j = 0; $j < $playeramount + 5; $j++) {
+          echo '<td>Zelleninhalt</td>';               <!--Hier sollen die Figurenbilder später angezeigt werden  -->
+    }
+    echo '</tr>';
+}
+?>
+</table>
