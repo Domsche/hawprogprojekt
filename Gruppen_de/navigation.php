f6741654700c25490f66c34b873bf67d4e1add9e
Domsche <stecher.do@gmail.com>
2016-09-29 18:33:14 +0200

    pfad korrektur

--- Gruppen_de/navigation.php
+++ Gruppen_de/navigation.php
@@ -3,7 +3,7 @@
   <input type="checkbox" name="ton" value="an"> Ton an/aus<br>
 </form>
 
-<form action =mainmenu.php >
+<form action=Gruppen_de/mainmenu.php >
   <button>Zum Menü</button>
 </form>
  php>
