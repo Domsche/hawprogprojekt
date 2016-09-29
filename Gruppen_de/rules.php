80c9dfb491ae749c06a0d2303d2397b19023f658
Domsche <stecher.do@gmail.com>
2016-09-29 18:47:49 +0200

    Erster Entwurf

--- Gruppen_de/rules.php
+++ Gruppen_de/rules.php
@@ -1,1 +1,14 @@
+<?php
+<a href="http://www.kosmos.de/_files_media/mediathek/downloads/anleitungen/1398/targi.pdf">Hier findest du die Regeln</a>
 
+
+			<form action=Gruppen_de/MainMenu.php>
+				<button>Zurück zum Menü</button>				
+			</form>
+  
+  
+			<form action=Gruppen_de/field.php>
+				<button>Zurück zum Spiel</button>				
+			</form>
+  
+  php>
