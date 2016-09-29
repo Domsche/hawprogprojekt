c43058db6ee2f742aa31fedfbb353ea4798e923c
Domsche <stecher.do@gmail.com>
2016-09-29 18:44:01 +0200

    Erster Entwurf

--- Gruppen_de/ingamemenu.php
+++ Gruppen_de/ingamemenu.php
@@ -1,1 +1,11 @@
-
+<?php
+<form action=Gruppen_de/rules.php>
+				<button>Regeln</button>				
+</form>
+  
+<form action=Gruppen_de/MainMenu.php>
+				<button>Spiel Beenden</button>	
+</form>
+ 
+ php>
+  
