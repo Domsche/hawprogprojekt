320a605ffc976109a3b076b329d6ae091ff64bcc
Domsche <stecher.do@gmail.com>
2016-10-10 15:43:25 +0200

    Gruppen Include statt html code

--- StartingGame.php
+++ StartingGame.php
@@ -10,20 +10,9 @@
 	    <header>
 	    	<h1>Spiel Starten</h1>
 	    	</header>
-	    <!-- Noch nicht identisch zu strukt.html -->
-        <p>Spielmodus:<br>
-        <input type="button" name="playmode" value="KIPlayer" checked>Computer Gegner hinzufügen<br>
-        <input type="button" name="playmode" value="extraPlayer">Spieler hinzufügen<br></p>
-        <p>
-          <input type="radio" name="difficulty" value="easy" checked>Leicht<br><!-- Dieser Radio-Button ist später nur anwählbar wenn man KIPlayer gewählt hat -->
-          <input type="radio" name="difficulty" value="hard">Schwer<br>
-          </p>
-		  <article>
-		      <form>
-		        <!-- Hier sollen Spieler hinzugefügt werden -->
-		      </form>
-		      <button type="submit" value="Submit">Submit</button>
-		  </article>
+		  
+		 <?php include("Gruppen_de/newgame.php") ?>
+	  	 <?php include("Gruppen_de/tournament.php") ?>
 		</main>
 		
 	</body>
