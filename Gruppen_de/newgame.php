dca5e1d2f93b308f5c6dd8d717ca050227a1df1f
Domsche <stecher.do@gmail.com>
2016-10-10 16:38:51 +0200

    hinzufügen von breaks

--- Gruppen_de/newgame.php
+++ Gruppen_de/newgame.php
@@ -5,10 +5,10 @@ $db_connection = mysqli_connect(„localhost“, „root“, „“ , „startga
   <fieldset>
  <label for="newplayer">Neuer Spieler:</label> 
  <input type="text" id="newplayer" size="30" maxlength="30" value="Name"> <br>
-    <input type="enemytype" id="spieler" name="Gegnertyp" value="spieler">
-    <label for="spieler"> Spieler</label><br> 
-    <input type="enemytype" id="ki" name="Gegnertyp" value="ki">
-    <label for="ki"> KI-Gegner</label><br> 
+    <input type="enemytype" id="spieler" name="Gegnertyp" value="spieler"><br>
+    <label for="spieler"> Spieler</label><br> <br>
+    <input type="enemytype" id="ki" name="Gegnertyp" value="ki"><br>
+    <label for="ki"> KI-Gegner</label><br> <br>
  </fieldset>
 </form>
    <!-- Das Formular soll später mit Javascript so verarbeitet werden, dass ein neues Eingabefeld erscheint -->
