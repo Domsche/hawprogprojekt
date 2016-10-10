cf32de179cb7f82393de4476383af1a9eb7f670a
Domsche <stecher.do@gmail.com>
2016-10-10 15:02:16 +0200

    Aufnahme meiner SQL-Datenbank

--- Gruppen_de/registration.php
+++ Gruppen_de/registration.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=TargiUsers', 'root', '');
 ?>
 <!DOCTYPE html> 
 <html> 
