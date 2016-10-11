059200646fa55dd25fb218a89c37e92cdbfb1935
Domsche <stecher.do@gmail.com>
2016-10-11 20:19:54 +0200

    tippfehler beseitigt

--- Gruppen_de/login.php
+++ Gruppen_de/login.php
@@ -2,7 +2,7 @@
 	<fieldset>
 		<legend>Bitte fülle die Log-In Felder aus</legend>
 		<label for=account>Benutzername:</label>
-		<input type=text id=account name=Benutzername required>
+		<input type=text id=account name=benutzername required>
 		
 		<label for=passwort>Passwort:</label>
 		<input type=password id=passwort name=passwort required></p>
@@ -11,7 +11,7 @@
 		<label for=login>Einloggen</label>
 		<input type=submit id=login>
 </form>
-</form>
+
   <form action=Gruppen_de/registration.php>
   	<button>Neu Registrieren</button>				
   </form>
