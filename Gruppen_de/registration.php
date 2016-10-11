10d75669385c309d2f1d10031f67ce4f11485e7a
Domsche <stecher.do@gmail.com>
2016-10-11 20:16:26 +0200

    Neuer Ansatz

--- Gruppen_de/registration.php
+++ Gruppen_de/registration.php
@@ -1,85 +1,25 @@
-<?php 
-session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=TargiUsers', 'root', '');
-?>
-<!DOCTYPE html> 
-<html> 
-<head>
-  <title>Registrierung</title>	
-</head> 
-<body>
- 
-<?php
-$showFormular = true; //Prüfen der Eingaben und übergabe der Inhalte
- 
-if(isset($_GET['register'])) {
-	$error = false;
-	$email = $_POST['email'];
-	$passwort = $_POST['passwort'];
-	$passwort2 = $_POST['passwort2'];
+<form method=POST action=form_eval_registration.php>
+	<fieldset>
+		<legend>Neu Registrierung</legend>
 	
-	if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
-		$error = true;
-	} 	
-	if(strlen($passwort) == 0) {
-		echo 'Bitte ein Passwort angeben<br>';
-		$error = true;
-	}
-	if($passwort != $passwort2) {
-		echo 'Die Passwörter müssen übereinstimmen<br>';
-		$error = true;
-	}
-	
-	//Prüfung ob Email bereits vorhanden
-	if(!$error) { 
-		$statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-		$result = $statement->execute(array('email' => $email));
-		$user = $statement->fetch();
+			<label for=user>Benutzername: </label>
+			<input type=text id=username name=registration required>
 		
-		if($user !== false) {
-			echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
-			$error = true;
-		}	
-	}
-	
-	//Fehlerfreie Anmeldung
-	if(!$error) {	
-		$passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
-		
-		$statement = $pdo->prepare("INSERT INTO users (email, passwort) VALUES (:email, :passwort)");
-		$result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash));
-		
-		if($result) {		
-			echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
-			$showFormular = false;
-		} else {
-			echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
-		}
-	} 
-}
- 
-if($showFormular) {
-?>
- 
-<form action="?register=1" method="post">
-E-Mail:<br>
-<input type="email" size="40" maxlength="250" name="email"><br><br>
- 
-Dein Passwort:<br>
-<input type="password" size="40"  maxlength="250" name="passwort"><br>
- 
-Passwort wiederholen:<br>
-<input type="password" size="40" maxlength="250" name="passwort2"><br><br>
- 
-<input type="submit" value="Bestätigen">
-</form>
- 
-<?php
-} //Ende von if($showFormular)
-?>
- 
-</body>
-</html>
+			
+			<label for=passwort>Passwort:</label>
+			<input type=text id=passwort name=registration required> 
 
-<!-- Quelle: https://www.php-einfach.de/experte/php-codebeispiele/loginscript/ -->
+			
+			<label for=passwort2>Passwort wiederholen:</label>
+			<input type=text id=passwort2 name=registration required>
+			
+			
+			<label for=email>E-Mail Adresse:</label>
+			<input type=email id=email name=registration required>
+			
+	</fieldset>
+	
+	<label for=submit>Abschicken</label>
+	<input type=submit id=submit>
+	
+</form>		
