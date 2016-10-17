bae0721defc8e52b38477c085218d26e61c58a3b
Domsche <stecher.do@gmail.com>
2016-10-17 20:57:38 +0200

    Anpassung an neue strukt

--- Gruppen_de/rules.php
+++ Gruppen_de/rules.php
@@ -1,14 +1,2 @@
-<?php
 <a href="http://www.kosmos.de/_files_media/mediathek/downloads/anleitungen/1398/targi.pdf">Hier findest du die Regeln</a>
-
-
-			<form action=Gruppen_de/MainMenu.php>
-				<button>Zurück zum Menü</button>				
-			</form>
-  
-  
-			<form action=Gruppen_de/field.php>
-				<button>Zurück zum Spiel</button>				
-			</form>
-  
-  php>
+<p> Autor original Regeln:  Andreas Steiger</p>
