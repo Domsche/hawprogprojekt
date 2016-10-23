4eb67629b4561a26075c03b49e0dd68f0920bbf7
Domsche <stecher.do@gmail.com>
2016-10-23 15:03:41 +0200

    Aktuallisierung der Gruppennamen

--- PlayingSurface.php
+++ PlayingSurface.php
@@ -11,14 +11,14 @@
 	  	<h1>TARGI - THE GAME</h1>
 	    </header>
 	 		 <?php include("Gruppen_de/navigation.php") ?>
-	 		 <?php include("Gruppen_de/gamestats.php") ?>
-	  		 <?php include("Gruppen_de/field.php") ?>
+	 		 <?php include("Gruppen_de/spielstats.php") ?>
+	  		 <?php include("Gruppen_de/spielfeld.php") ?>
 	
 		<h2 align="center">Highscores</h2>		 
-			 <?php include("Gruppen_de/finishgame.php") ?>
+			 <?php include("Gruppen_de/abrechnung.php") ?>
 		  
 		<footer>
-			<?php include("Gruppen_de/rules.php") ?>
+			<?php include("Gruppen_de/regeln.php") ?>
 		</footer>
          </main>
 	</body>
