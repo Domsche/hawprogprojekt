7706fbfdd7264b37d069c7bb8632a3433731c85c
Domsche <stecher.do@gmail.com>
2016-10-23 14:02:30 +0200

    Einfügen der letzten Elemente

--- Gruppen_de/gamestats.php
+++ Gruppen_de/gamestats.php
@@ -1,12 +1,15 @@
 <?php
-$playeramount[] = 0 <!--Startwert ist 0, pro Spieler +1  -->
-$playerName[] = "KI-Gegner"  <!-- In diesem Array sollen Spielernamen gespeichert/übergeben werden, KI-Gegner bei keiner Namensangabe -->
+  $playeramount[] = 0;<!--Startwert ist 0, pro Spieler +1  -->
+  $playerName[] = "KI-Gegner"; <!-- In diesem Array sollen Spielernamen gespeichert/übergeben werden, KI-Gegner bei keiner Namensangabe -->
   
-$bankDatteln[] = 3      <!-- Das Array soll die Lagerbestände der einzelnen Spieler speichern, 3 ist Startwert -->
-$bankPfeffer[] = 3
-$bankSalz[] = 3
-$bankDatteln[] = 3
+  $bankDatteln[] = 3;    <!-- Das Array soll die Lagerbestände der einzelnen Spieler speichern, 3 ist Startwert -->
+  $bankPfeffer[] = 3;
+  $bankSalz[] = 3;
+  $bankDatteln[] = 3;
+  $bankSiegpunkte[] = 3
   
+    
+    <!-- Ausgabe der Waren des Spielers -->
   <table>
  <tr>
    <th>Spieler</th>
@@ -25,7 +28,7 @@ foreach  ($i = 0; $i < $playeramount; $i++) {    <!-- Schleife zum auslesen der 
 </table>
 
 for ($i = 0; $i < $playeramount; $i++) {
-  echo "$playerName[i]"                                 <!-- Grundgerüst 3x5 für Gebäudekarten. Zellen werden in Typfarbe später eingefärbt -->
+  echo "$playerName[i]";                                 <!-- Grundgerüst 3x5 für Gebäudekarten. Zellen werden in Typfarbe später eingefärbt -->
   <table>
     <tr>
       <td></td>
@@ -55,5 +58,16 @@ for ($i = 0; $i < $playeramount; $i++) {
     
   </table>
  ?>
- <!-- Elemente 66-67 Folgen! -->
+
+<!-- Hier folgt der Spiellog/Chat eingabe -->
+$actionmessage[]; <!-- wird aus Datenbank übergeben -->
+$chatmessage[];
+<form>
+		<label for=chat>Message:</label>
+		<input type=textarea name=chatmessage id=chat>
+    <button name="absenden">Senden</button>
+</form>
+<?php
+   <!-- Hier würde würde eine Php-Funktion das chatmessage array über eine echo-Schleife ausgeben, hierfür benötige ich nach Recherche ajax.php (wegen serverseitig / clientseitiger Buttonclick).
+     Muss ich mich noch in dieses Thema einarbeiten oder genügt die Beschreibung vorerst? -->
   
