22a65b626e64132412a4ebdb42ebcd2dae55e358
Domsche <stecher.do@gmail.com>
2016-10-27 14:50:11 +0200

    Übernahme der Ton an/aus checkbox

--- Gruppen_de/navigation.php
+++ Gruppen_de/navigation.php
@@ -1,6 +1,8 @@
-<form>
-  <input type="checkbox" name="ton" value="an"> Ton an/aus<br>
-</form>
+<form action="hauptmenu.php">
+	<input type="checkbox" name="ton" value="an"> Ton an/aus<br>
+ 	<input type="submit" value="Submit">Abschicken<br>
+</form>	  
+
 
 <form action=Gruppen_de/mainmenu.php >
   <button>Zum Menü</button>
