fa7487c4b3f009e121cbe92e10508d2290b86b30
Domsche <stecher.do@gmail.com>
2016-10-27 14:49:08 +0200

    Anpassung der Checkbox

--- Gruppen_de/hauptmenu.php
+++ Gruppen_de/hauptmenu.php
@@ -3,11 +3,13 @@
 </form>
 
 <form action=LogOut>
+	<button>Log-Out</button>
           //Diese Ansicht habe ich noch nicht erstellt, wird nachgeholt
 </form>
-
-<form>
-  <input type="checkbox" name="ton" value="an"> Ton an/aus<br>
-</form>
+	
+<form action="hauptmenu.php">
+	<input type="checkbox" name="ton" value="an"> Ton an/aus<br>
+ 	<input type="submit" value="Submit">Abschicken<br>
+</form>	  
 
 
